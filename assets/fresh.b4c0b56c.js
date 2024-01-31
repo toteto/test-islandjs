d3db98f894349eb4f064b7ff8e677f380801ca9f
toteto <toteto@users.noreply.github.com>
2024-01-31 19:39:34 +0000

    Deploying to gh-pages from @ toteto/test-islandjs@8f225770dbff3c5e0396f512149f50484ccfe9a9 🚀

---
+++ assets/fresh.b4c0b56c.js
@@ -0,0 +1,4 @@
+import{jsx as t,jsxs as r,Fragment as s}from"react/jsx-runtime";const a=void 0,h=[],i="Fresh";function c(n){const e=Object.assign({h1:"h1",a:"a",p:"p"},n.components);return r(s,{children:[r(e.h1,{id:"fresh",children:[t(e.a,{className:"header-anchor","aria-hidden":"true",href:"#fresh",children:"#"}),"Fresh"]}),`
+`,t(e.p,{children:"\u{1F9C3}"})]})}function d(n={}){const{wrapper:e}=n.components||{};return e?t(e,Object.assign({},n,{children:t(c,n)})):c(n)}const f="2024/1/31 19:39:02",l=`# Fresh
+
+\u{1F9C3}`;export{l as content,d as default,a as frontmatter,f as lastUpdatedTime,i as title,h as toc};
