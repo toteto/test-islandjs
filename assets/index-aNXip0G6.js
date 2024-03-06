2bf611e39d696b5c25176f3856c07a457878b956
Antonio Ivanovski <toteto@users.noreply.github.com>
2024-03-06 12:51:20 +0100

    test4

---
+++ assets/index-aNXip0G6.js
@@ -0,0 +1,19 @@
+import{jsx as n,jsxs as r,Fragment as s}from"react/jsx-runtime";const i={title:"Identity and Access Management - Overview - Vidos"},o=[{id:"features",text:"Features",depth:2}],d="IAM Service Overview";function t(a){const e=Object.assign({h1:"h1",a:"a",p:"p",h2:"h2",ul:"ul",li:"li",strong:"strong"},a.components);return r(s,{children:[r(e.h1,{id:"iam-service-overview",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#iam-service-overview",children:"#"}),"IAM Service Overview"]}),`
+`,n(e.p,{children:"The IAM (Identity and Access Management) service is a crucial component of our system. It allows you to manage access to your resources in a secure manner."}),`
+`,r(e.h2,{id:"features",children:[n(e.a,{className:"header-anchor","aria-hidden":"true",href:"#features",children:"#"}),"Features"]}),`
+`,r(e.ul,{children:[`
+`,r(e.li,{children:[n(e.strong,{children:"Fine grain access Control"}),": Define who has what kind of access to which resources."]}),`
+`,r(e.li,{children:[n(e.strong,{children:"API Key Management"}),": Generate and manage API keys for accessing your resources programmatically."]}),`
+`]})]})}function h(a={}){const{wrapper:e}=a.components||{};return e?n(e,Object.assign({},a,{children:n(t,a)})):t(a)}const l="Invalid Date",u=`---
+title: 'Identity and Access Management - Overview - Vidos'
+---
+
+# IAM Service Overview
+
+The IAM (Identity and Access Management) service is a crucial component of our system. It allows you to manage access to your resources in a secure manner.
+
+## Features
+
+-   **Fine grain access Control**: Define who has what kind of access to which resources.
+-   **API Key Management**: Generate and manage API keys for accessing your resources programmatically.
+`;export{u as content,h as default,i as frontmatter,l as lastUpdatedTime,d as title,o as toc};
